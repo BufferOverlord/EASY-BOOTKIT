4f7e8b51cc40f029a2f4a624177f40576dbaf378
BufferOverlord <110816772+BufferOverlord@users.noreply.github.com>
2025-01-15 01:26:00 +0100

    Update EFI_MAIN.c

--- EFI_MAIN.c
+++ EFI_MAIN.c
@@ -4,72 +4,6 @@
 EFI_SYSTEM_TABLE* GlobalSystemTable = NULL;
 
 
-
-EFI_STATUS
-EFIAPI
-SearchMemorySignatureInFirst2GB(EFI_SYSTEM_TABLE* SystemTable, UINT8* Signature, UINTN SignatureSize) {
-    EFI_STATUS Status;
-    EFI_MEMORY_DESCRIPTOR* MemoryMap;
-    EFI_MEMORY_DESCRIPTOR* MemoryDescriptor;
-    UINTN MemoryMapSize = 0;
-    UINTN MapKey;
-    UINTN DescriptorSize;
-    UINT32 DescriptorVersion;
-    UINTN Index;
-    UINTN MaxSearchSize = 2 * 1024 * 1024 * 1024;
-
-    Status = SystemTable->BootServices->GetMemoryMap(
-        &MemoryMapSize, NULL, &MapKey, &DescriptorSize, &DescriptorVersion);
-
-    if (EFI_ERROR(Status)) {
-        return Status;
-    }
-
-    MemoryMap = AllocatePool(MemoryMapSize);
-    if (MemoryMap == NULL) {
-        return EFI_OUT_OF_RESOURCES;
-    }
-
-    Status = SystemTable->BootServices->GetMemoryMap(
-        &MemoryMapSize, MemoryMap, &MapKey, &DescriptorSize, &DescriptorVersion);
-
-    if (EFI_ERROR(Status)) {
-        FreePool(MemoryMap);
-        return Status;
-    }
-
-    MemoryDescriptor = (EFI_MEMORY_DESCRIPTOR*)MemoryMap;
-    for (Index = 0; Index < MemoryMapSize / DescriptorSize; Index++) {
-        EFI_MEMORY_TYPE MemType = MemoryDescriptor->Type;
-        EFI_PHYSICAL_ADDRESS MemStart = MemoryDescriptor->PhysicalStart;
-        UINTN MemSize = MemoryDescriptor->NumberOfPages * EFI_PAGE_SIZE;
-
-        if (MemStart < MaxSearchSize && MemType != EfiConventionalMemory && MemType != EfiBootServicesData) {
-            for (UINTN Offset = 0; Offset < MemSize - SignatureSize; Offset++) {
-                UINT8* Addr = (UINT8*)(MemStart + Offset);
-                if (CompareMem(Addr, Signature, SignatureSize) == 0) {
-                    FreePool(MemoryMap);
-                    return EFI_SUCCESS;
-                }
-            }
-        }
-
-        MemoryDescriptor = (EFI_MEMORY_DESCRIPTOR*)((UINT8*)MemoryDescriptor + DescriptorSize);
-    }
-
-    FreePool(MemoryMap);
-    return EFI_NOT_FOUND;
-}
-
-
-
-
-
-
-
-
-
-
 static const EFI_GUID ProtocolGuid
 = { 0x1a32111a, 0xee4f, 0x1826, {0x4b, 0x1a, 0x40, 0xb7, 0xff, 0x7f, 0x00, 0xf5} };
 
@@ -98,18 +32,9 @@ static EFI_STATUS EFIAPI HookedExitBootServices(EFI_HANDLE ImageHandle, UINTN Ma
     gBS->ExitBootServices = OriginalExitBootServices;
     EFI_STATUS Status;
     UINT8 Signature[] = { 0x48, 0xB8, 0x77, 0xBE, 0x9F, 0x1A, 0x2F, 0xDD, 0x24, 0x06, 0x49, 0xF7, 0xE1 };
-    UINTN SignatureSize = sizeof(Signature);
-
-    Status = SearchMemorySignatureInFirst2GB(GlobalSystemTable, Signature, SignatureSize);
-
-    if (EFI_ERROR(Status)) {
+    
         GlobalSystemTable->ConOut->SetAttribute(GlobalSystemTable->ConOut, EFI_BACKGROUND_RED | EFI_WHITE);
-    }
-    else {
         GlobalSystemTable->ConOut->SetAttribute(GlobalSystemTable->ConOut, EFI_BACKGROUND_GREEN | EFI_WHITE);
-    }
-
-
 
 
 
